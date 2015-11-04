0b0c1aa7faf201852f92f3df54fb71f952b6f540
Killer <leopardoooo@163.com>
2015-11-04 14:35:57 +0800

    "我的"模块Controller定义

---
+++ icar/MyPrivateViewController.m
@@ -0,0 +1,39 @@
+//
+//  MyPrivateViewController.m
+//  icar
+//
+//  Created by Killer on 15/11/4.
+//  Copyright (c) 2015年 杭州聚轮网络科技有限公司. All rights reserved.
+//
+
+#import "MyPrivateViewController.h"
+
+@interface MyPrivateViewController ()
+
+@end
+
+@implementation MyPrivateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
